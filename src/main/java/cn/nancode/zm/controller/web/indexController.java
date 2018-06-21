c9981231e643bb3001561c10f12cb27882478d7f
nan <50326921@qq.com>
2018-06-21 14:06:02 +0800

    no message

--- src/main/java/cn/nancode/zm/controller/web/indexController.java
+++ src/main/java/cn/nancode/zm/controller/web/indexController.java
@@ -1,5 +1,7 @@
 package cn.nancode.zm.controller.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,8 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexController {
 
+    private Logger logger = LoggerFactory.getLogger((getClass()));
+
     @GetMapping("/")
     public String index() {
+
+        logger.error("首页错误哦 哦哦哦哦哦哦哦哦哦哦");
+        
         return "index";
     }
 }
